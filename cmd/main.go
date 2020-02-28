c59fc8f898d8d02025781f09b579c1e6c2d97d36
Zam <love8879201@gmail.com>
2020-02-29 01:38:27 +0800

    reply first activity tile

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -11,6 +12,7 @@ import (
 )
 
 var bot *linebot.Client
+users := make(map[string]string)
 
 // TaishinEvent store taishin activities information
 type TaishinEvent struct {
@@ -46,11 +48,6 @@ func handler(writer http.ResponseWriter, request *http.Request) {
 	fmt.Fprintf(writer, "This is the service for Activity Helper. by Zam")
 }
 
-// User store information for Taishin bank
-type User struct {
-	ID string `json:"id"`
-}
-
 func handleEvents(writer http.ResponseWriter, request *http.Request) {
 	events, err := bot.ParseRequest(request)
 	if err != nil {
@@ -65,11 +62,7 @@ func handleEvents(writer http.ResponseWriter, request *http.Request) {
 		fmt.Println("fmt event: ", event)
 		fmt.Println("fmt event Source: ", event.Source)
 		if event.Type == linebot.EventTypeMessage {
-			userID := event.Source.UserID
-			groupID := event.Source.GroupID
-			roomID := event.Source.RoomID
-
-			fmt.Println("userID", userID, "groupID", groupID, "roomID", roomID)
+			users
 			switch message := event.Message.(type) {
 			case *linebot.TextMessage:
 				handleText(message, event.ReplyToken)
@@ -79,17 +72,39 @@ func handleEvents(writer http.ResponseWriter, request *http.Request) {
 }
 
 func handleText(message *linebot.TextMessage, replyToken string) {
-	registerTaishinActivities(message, replyToken)
-	// if message.Text == "註冊" {
-	// }
+	if message.Text == "註冊" {
 
-	// if message.Text == "活動" {
+	}
 
-	// }
+	if message.Text == "活動" {
+		registerTaishinActivities(message, replyToken)
+	}
+}
+
+func getTaishinActivities(message *linebot.TextMessage, replyToken string) {
+	out, err := exec.Command("python", "ActivityHelper.py", "get", "-u", message.Text).Output()
+
+	if err != nil {
+		fmt.Println(err.Error())
+		// 	_, err := bot.ReplyMessage(replyToken, linebot.NewTextMessage("py err")).Do()
+		// 	if err != nil {
+		// 		fmt.Println("fail to send message")
+		// 	}
+	} else {
+		var events []TaishinEvent
+		json.Unmarshal(out, &events)
+		fmt.Println(events)
+		if len(events) != 0 {
+			_, err := bot.ReplyMessage(replyToken, linebot.NewTextMessage(events[0].Title)).Do()
+			if err != nil {
+				fmt.Println("fail to send message")
+			}
+		}
+	}
 }
 
 func registerTaishinActivities(message *linebot.TextMessage, replyToken string) {
-	out, err := exec.Command("python", "ActivityHelper.py", "get", "-u", message.Text).Output()
+	out, err := exec.Command("python", "ActivityHelper.py", "register", "-u", message.Text).Output()
 
 	fmt.Println(string(out))
 	if err != nil {
